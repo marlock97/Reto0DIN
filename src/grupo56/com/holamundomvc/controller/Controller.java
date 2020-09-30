2f3c2a033780992493089feb0e69a2bae1fa1411
lorena <2dam@HZ359575.TartangaLH.eus>
2020-09-30 12:43:05 +0200

    param model and view

--- src/grupo56/com/holamundomvc/controller/Controller.java
+++ src/grupo56/com/holamundomvc/controller/Controller.java
@@ -5,10 +5,24 @@
  */
 package grupo56.com.holamundomvc.controller;
 
+import grupo56.com.holamundomvc.model.Model;
+import grupo56.com.holamundomvc.view.View;
+
 /**
  *
- * @author 2dam
+ * @author Martin programacion a pares roles: conductor: lorena observador:
+ * martin
  */
 public class Controller {
+
+    /*
+     * shows the greeting from the model
+     * @param model model to use
+     * @param view view to use
+     */
     
+    public void run(Model model, View view) {
+        view.showGreeting(model.getGreeting());
+    }
+
 }
